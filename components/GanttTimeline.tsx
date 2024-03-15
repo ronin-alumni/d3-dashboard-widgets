73bad90dd476f377dd0438dc1015e63a9938cade
Brooks Boyd <bro@oks.me>
2024-03-15 01:04:24 -0500

    Draw initial grid and axis labels

--- components/GanttTimeline.tsx
+++ components/GanttTimeline.tsx
@@ -4,13 +4,29 @@ import * as d3 from 'd3'
 const chartConfig = {
   height: 400,
   width: 1200,
+  margin: {
+    headerMonths: 20,
+    headerDays: 40,
+  },
 }
 const MS_IN_ONE_DAY = 1000 * 60 * 60 * 24
+const monthLabel = d3.utcFormat('%b %Y')
 
 function daysShift(start: Date, daysDelta: number): Date {
   const d = new Date(start.getTime())
   d.setDate(d.getDate() + daysDelta)
   return d
+}
+
+/**
+ * Modular components that are responsible for rendering portions of the visualization
+ */
+export interface D3ZonePainter {
+  getHeight: () => number
+  paintData: (
+    container: d3.Selection<SVGGElement, null, any, any>,
+    x: d3.ScaleTime<Date, number>,
+  ) => void
 }
 
 export default function GantTimeline() {
@@ -38,19 +54,53 @@ export default function GantTimeline() {
     chartConfig.width = svg.node()!.clientWidth
     initialX.range([0, chartConfig.width])
 
-    // Draw initial axis
+    // Draw initial inteface elements
     const dayTicks = initialX.ticks(d3.timeDay)
+    const dayWidth = initialX(dayTicks[1]) - initialX(dayTicks[0])
     svg
       .select('#grid')
       .selectAll<SVGLineElement, Date>('line')
       .data(dayTicks, d => d.getTime())
       .join('line')
-      .attr('style', 'stroke: #CCC; stroke-width: 1px')
       .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
+      .attr('style', 'stroke: #CCC; stroke-width: 1px')
       .attr('x1', 0)
       .attr('x2', 0)
-      .attr('y1', 0)
+      .attr('y1', chartConfig.margin.headerMonths + 5)
       .attr('y2', chartConfig.height)
+    svg
+      .select('#axis')
+      .selectAll<SVGGElement, Date>('g.day')
+      .data(dayTicks, d => d.getTime())
+      .join('g')
+      .attr('class', 'day')
+      .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
+      .append('text')
+      .attr('x', dayWidth / 2)
+      .attr('text-anchor', 'middle')
+      .attr('y', chartConfig.margin.headerDays)
+      .text(d => d.getDate())
+
+    const monthTicks = initialX.ticks(d3.timeMonth)
+    svg
+      .select('#axis')
+      .selectAll<SVGGElement, Date>('g.month')
+      .data(monthTicks, d => d.getTime())
+      .join('g')
+      .attr('class', 'month')
+      .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
+      .call(g => {
+        g.append('line')
+          .attr('style', 'stroke: #AAA; stroke-width: 2px')
+          .attr('x1', 0)
+          .attr('x2', 0)
+          .attr('y1', 0)
+          .attr('y2', chartConfig.height)
+        g.append('text')
+          .attr('x', 5)
+          .attr('y', chartConfig.margin.headerMonths)
+          .text(d => monthLabel(d))
+      })
 
     // Draw initial data into zones
   }
