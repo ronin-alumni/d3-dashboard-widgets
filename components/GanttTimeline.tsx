ff7ff7b03ab859b9861738a1af831d4ae0b86034
Brooks Boyd <bro@oks.me>
2024-03-17 10:25:34 -0500

    Create data for example Gantt visualization

--- components/GanttTimeline.tsx
+++ components/GanttTimeline.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react'
 import * as d3 from 'd3'
+import styles from './GanttTimeline.module.css'
 
 const chartConfig = {
   height: 400,
@@ -7,9 +8,13 @@ const chartConfig = {
   margin: {
     headerMonths: 20,
     headerDays: 40,
+    zone: 5,
+  },
+  padding: {
+    zone: 10,
   },
 }
-const MS_IN_ONE_DAY = 1000 * 60 * 60 * 24
+
 const monthLabel = d3.utcFormat('%b %Y')
 
 function daysShift(start: Date, daysDelta: number): Date {
@@ -22,14 +27,17 @@ function daysShift(start: Date, daysDelta: number): Date {
  * Modular components that are responsible for rendering portions of the visualization
  */
 export interface D3ZonePainter {
+  data: any[]
+  title: string
+  init: () => Promise<boolean>
   getHeight: () => number
   paintData: (
     container: d3.Selection<SVGGElement, null, any, any>,
-    x: d3.ScaleTime<Date, number>,
-  ) => void
+    x: d3.ScaleTime<number, number>,
+  ) => Promise<boolean>
 }
 
-export default function GantTimeline() {
+export default function GantTimeline({ zones }: { zones: D3ZonePainter[] }) {
   const svgRef = useRef<SVGSVGElement | null>(null)
 
   // Create D3 scale for initial state of the visualization
@@ -54,20 +62,22 @@ export default function GantTimeline() {
     chartConfig.width = svg.node()!.clientWidth
     initialX.range([0, chartConfig.width])
 
-    // Draw initial inteface elements
+    // Paint initial inteface elements
     const dayTicks = initialX.ticks(d3.timeDay)
     const dayWidth = initialX(dayTicks[1]) - initialX(dayTicks[0])
+    // Paint lines between each day
     svg
       .select('#grid')
       .selectAll<SVGLineElement, Date>('line')
       .data(dayTicks, d => d.getTime())
       .join('line')
       .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
-      .attr('style', 'stroke: #CCC; stroke-width: 1px')
+      .attr('style', d => `stroke: ${d.getDay() == 0 ? '#555' : '#CCC'}; stroke-width: 1px`) // Draw darker lines to start each week
       .attr('x1', 0)
       .attr('x2', 0)
       .attr('y1', chartConfig.margin.headerMonths + 5)
       .attr('y2', chartConfig.height)
+    // Paint numbers for each calendar day
     svg
       .select('#axis')
       .selectAll<SVGGElement, Date>('g.day')
@@ -76,11 +86,13 @@ export default function GantTimeline() {
       .attr('class', 'day')
       .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
       .append('text')
+      .attr('class', styles.headerDays)
       .attr('x', dayWidth / 2)
       .attr('text-anchor', 'middle')
       .attr('y', chartConfig.margin.headerDays)
       .text(d => d.getDate())
 
+    // Paint thicker lines and labels for each month
     const monthTicks = initialX.ticks(d3.timeMonth)
     svg
       .select('#axis')
@@ -91,18 +103,85 @@ export default function GantTimeline() {
       .attr('transform', d => `translate(${Math.floor(initialX(d))},0)`)
       .call(g => {
         g.append('line')
-          .attr('style', 'stroke: #AAA; stroke-width: 2px')
+          .attr('style', 'stroke: #555; stroke-width: 2px')
           .attr('x1', 0)
           .attr('x2', 0)
           .attr('y1', 0)
           .attr('y2', chartConfig.height)
         g.append('text')
+          .attr('class', styles.headerMonths)
           .attr('x', 5)
           .attr('y', chartConfig.margin.headerMonths)
           .text(d => monthLabel(d))
       })
 
+    // Initialize zones
+    await Promise.all(zones.map(z => z.init()))
+
     // Draw initial data into zones
+    const zoneWrapper = svg.select('#zones')
+    zoneWrapper.selectAll('*').remove()
+
+    // How tall is the data plot of each zone?
+    const heights = zones.map(z => z.getHeight())
+
+    // Track heights as we draw down the visualization
+    const cumulativeOffset: number[] = []
+    const zoneHeaderHeight = chartConfig.padding.zone + 14
+    await Promise.all(
+      zones.map((z, i) => {
+        // Create empty container for this zone
+        const c = zoneWrapper.append('g').attr('id', `zone-${i}`)
+
+        // For this chart type, each zone gets a boundary and a title (as an example, to be visible where the containers are), drawn here by the chart controller.
+        // Since it is the same for each zone, it's done here rather than requiring boilerplate in each zone definition
+
+        // Position the zone container
+        const priorZoneHeight =
+          i == 0
+            ? 0
+            : zoneHeaderHeight +
+              chartConfig.padding.zone +
+              heights[i - 1] +
+              chartConfig.padding.zone +
+              chartConfig.margin.zone
+        const verticalOffset = i == 0 ? 0 : cumulativeOffset[i - 1] + priorZoneHeight
+        cumulativeOffset[i] = verticalOffset
+        c.attr('transform', `translate(0,${verticalOffset + chartConfig.margin.headerDays + 10})`)
+
+        // Draw boundary around the zone
+        c.append('rect')
+          .attr('class', styles.zoneBg)
+          .attr('x', chartConfig.margin.zone)
+          .attr('y', 0)
+          .attr('width', chartConfig.width - chartConfig.margin.zone * 2)
+          .attr(
+            'height',
+            zoneHeaderHeight + chartConfig.padding.zone + heights[i] + chartConfig.padding.zone,
+          )
+        // Draw title for the zone
+        c.append('rect')
+          .attr('x', chartConfig.margin.zone)
+          .attr('y', 0)
+          .attr('width', chartConfig.width - chartConfig.margin.zone * 2)
+          .attr('height', zoneHeaderHeight)
+          .attr('style', 'fill: #000; fill-opacity: 0.1')
+        c.append('text')
+          .attr('class', styles.headerZone)
+          .attr('x', chartConfig.padding.zone)
+          .attr('y', zoneHeaderHeight - 7)
+          .text(z.title)
+
+        // Container for actual data needs to have a translate X component of zero, so that the Scale has a proper result
+        return z.paintData(
+          c
+            .append('g')
+            .attr('class', 'data')
+            .attr('transform', `translate(0, ${zoneHeaderHeight + chartConfig.padding.zone})`),
+          initialX,
+        )
+      }),
+    )
   }
 
   useEffect(() => {
@@ -113,7 +192,7 @@ export default function GantTimeline() {
   return (
     <svg ref={svgRef} style={{ width: '100%', height: '400px', background: '#FFC' }}>
       <g id="grid" />
-      <g id="data" />
+      <g id="zones" />
       <g id="axis" />
     </svg>
   )
